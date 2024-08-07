94d856f41766b34e4e9300a02747a889bb81fe9c
Keyvan Mahmoudi <keyvanmahmoudi72@gmail.com>
2024-08-07 11:41:41 +0200

    fix

--- index.mjs
+++ index.mjs
@@ -3,27 +3,39 @@ import drone from './connect.mjs';
 
 const drone2 = drone('192.168.8.137', 8889);
 const drone1 = drone('192.168.8.120', 8889);
-// const drone3 = drone('192.168.8.142', 8889);
+const drone3 = drone('192.168.8.142', 8889);
 
-// const drones = [drone1, drone2, drone3];
-
-const executeCommandForAllDrones = (command) => {
-  drones.forEach((drone) => {
-    command(drone);
-  });
+const execute = async (...commands) => {
+  for (const command of commands) {
+    await command();
+    console.log('-------------------------------');
+  }
 };
-
-// const takeOff = async () => {
-
 drone1.connect();
-await drone1.sendCommand('takeoff');
-// };
-
-// const connectDrone = async () => {
-// };
-
-// connectDrone();
-// takeOff();
-
-// executeCommandForAllDrones(connectDrone);
-// executeCommandForAllDrones(takeOff);
+execute(
+  drone1.command.takeoff(),
+  // drone1.command.speed(75),
+  // drone1.command.go(0, 0, 30, 50, 2),
+  // drone1.command.forward(250),
+  // drone1.command.go(0, 0, 60, 50, 2),
+  // drone1.command.backward(250),
+  // drone1.command.go(0, 0, 200, 50, 2),
+  // drone1.command.forward(200),
+  // drone1.command.go(0, 0, 50, 50, 2),
+  // drone1.command.align(),
+  // drone1.command.align(),
+  // drone1.command.forward(50),
+  // drone1.command.align(),
+  // drone1.command.left(500),
+  // drone1.command.forward(150),
+  // drone1.command.right(200),
+  // drone1.command.go(0, 0, 150, 50, 7),
+  // drone1.command.right(180),
+  // drone1.command.align(),
+  // drone1.command.backward(420),
+  // drone1.command.align(),
+  // drone1.command.align(),
+  drone1.command.land()
+)
+  .catch((error) => console.log(error))
+  .finally(drone1.disconnect);
